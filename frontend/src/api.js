c511defd8de3d222b78904b4942b4037cddad1a9
Quinn Tran <>
2019-06-12 15:57:53 +1000

    minor changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom"
 
-export const baseURL = "http://localhost:3000";
+export const baseURL = "https://172.22.31.176:443";
+
 /**
  * Authentication
  * @param {*} email 
